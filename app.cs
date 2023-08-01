66bb3160e5ee7e5904821ba556a68828c02cbf52
James Attrill <attrill20@gmail.com>
2023-08-01 10:19:11 +0100

    started calc

--- app.cs
+++ app.cs
@@ -1,18 +1,16 @@
 using System; // allows classes from the system namespace
 // can anything else instead of System be used?
 
-namespace HelloWorld // used to declare a scope that contains related objects - this is a container
+namespace SimpleCalculator // used to declare a scope that contains related objects - this is a container
 // how / when would we access this namespace later?      
 {
     class Program // basically an object?
     {
         static void Main (string []args) // the Main method
         {
-            string name = "James";
-            console.WriteLine(name);
-            int age = 34;
-            console.WriteLine(age);
-            console.WriteLine ("Hello World"); // write to console
+            
+            Console.WriteLine ("Simple Calculator"); // write to console
+            Console.WriteLine ("---------------"); // write to console
         }
     }
     
